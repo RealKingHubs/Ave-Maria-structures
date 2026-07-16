26a4165d39fd82cb792f0d32b87624e8a1656ce8
RealKingHubs <odokingsleyu@gmail.com>
2026-07-16 11:25:27 +0100

    Chores: Real site pictures added and number modification

--- js/projects-data.js
+++ js/projects-data.js
@@ -25,25 +25,25 @@ export const PROJECTS = [
     description: "A Super Store built to full commercial code, for consumers"
   },
   {
-    id: "origho-renovation",
-    title: "Origho Court Renovation",
-    category: "renovation",
-    location: "Surulere, Lagos",
-    year: 2026,
-    status: "In progress",
-    image: "https://images.unsplash.com/photo-1753893558281-9acda0662bbd?q=80&w=1200&auto=format&fit=crop",
-    alt: "House exterior mid renovation with scaffolding",
-    description: "A full structural renovation of a 1990s family home, now mid-project."
+    id: "family House",
+    title: "Family House",
+    category: "residential",
+    location: "Eguta Olido, Enugu Ezike",
+    year: 2021,
+    status: "Completed",
+    image: "https://scontent-los4-1.xx.fbcdn.net/v/t39.30808-6/486094408_9794410313985088_6426099784399815918_n.jpg?stp=dst-jpg_tt6&cstp=mx960x720&ctp=s590x590&_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGXdC7xrWIvfX7KrjHS1oy0Sq_tzZYKLpRKr-3NlgoulJzQgcpdXZ9_kVsDJIh-w4bc87jCEuVSEPIipQbvri-E&_nc_ohc=cEAACAeJuoQQ7kNvwGk-hXP&_nc_oc=Adqq-9aP4zaTbzp5Atq37hzlWRX8FLcEK-nib8Suk8sj5aS-qOA2RgvBYT3t5Ze6ZXY&_nc_zt=23&_nc_ht=scontent-los4-1.xx&_nc_gid=Ns8bnhBrLCjkWZMGbYy9tg&_nc_ss=7b2a8&oh=00_AQBLwfsCsVR8A3VuygD7m3T2lenp7sqngMkqm2wcHLGTgQ&oe=6A5E7966",
+    alt: "Modern tile-fronted building facade",
+    description: "A 4-bedroom duplex delivered from foundation to finishing in fourteen months."
   },
   {
-    id: "ajah-frame",
-    title: "Ajah Estate Block",
-    category: "residential",
-    location: "Ajah, Lagos",
-    year: 2026,
+    id: "london-villa",
+    title: "London Villa hotel & Suites",
+    category: "commercial",
+    location: "Obollo, Enugu",
+    year: 2025,
     status: "In progress",
-    image: "https://images.unsplash.com/photo-1777919393730-463e2c0b7f4c?q=80&w=1200&auto=format&fit=crop",
-    alt: "Reinforced concrete structural frame under construction",
-    description: "Reinforced concrete frame for a 6-unit residential block, currently rising."
+    image: "https://scontent-los4-1.xx.fbcdn.net/v/t39.30808-6/591083999_783618614712708_5129720422551759805_n.jpg?stp=dst-jpg_tt6&cstp=mx864x1152&ctp=s864x1152&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHJShO7k3tfkK8PXcUuBZ3xq1OPbIqNJwKrU49sio0nAp1Hio-m8bQYpeK7A9LCkEK4jAO242IEBM5USS_tZZc3&_nc_ohc=BJmVazUCRUwQ7kNvwHipYXI&_nc_oc=AdqtSjHbsaWUKCocI17uw5UOWmDwD0uXN6zfqUrhNYm-bNbFF_eVXJtKdhuUb-RyaSk&_nc_zt=23&_nc_ht=scontent-los4-1.xx&_nc_gid=W6bVjV1tXrsUno_idC0GlQ&_nc_ss=7b2a8&oh=00_AQC9S5lcI-9puIHNt7QqFx9yDenM70lPlkvUk1mAdi5Wuw&oe=6A5E6FBE",
+    alt: "London Villa hotel",
+    description: "Commercial building with 10 rooms, bar, club house and a swimming pool."
   }
 ];
